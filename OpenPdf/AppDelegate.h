ecd658eb99facc425262b305f1652162b4f28b4d
pasha4uu <pasha7864u@gmail.com>
2018-10-07 23:46:05 +0530

    Initial Commit

---
+++ OpenPdf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenPdf
+//
+//  Created by PASHA on 07/10/18.
+//  Copyright © 2018 Pasha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
